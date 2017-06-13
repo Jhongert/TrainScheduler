89ff0b2f1632cdcd73a5e4fe1d2354774a513429
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-06-13 10:24:42 -0400

    update

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var config = {
+    apiKey: "AIzaSyC5dWsV-fEWvpDyXGQsM5SeeLeKmoeDaj8",
+    authDomain: "train-scheduler-b6366.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b6366.firebaseio.com",
+    projectId: "train-scheduler-b6366",
+    storageBucket: "train-scheduler-b6366.appspot.com",
+    messagingSenderId: "82721035089"
+  };
+  firebase.initializeApp(config);
+
+  
+});
