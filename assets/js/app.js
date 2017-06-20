6acba97d1b0af5dbe381499da135b57b8ec8be0e
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-06-19 22:57:58 -0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,30 @@ $(document).ready(function(){
   	};
   	firebase.initializeApp(config);
 
+    $("#sign-in").click(function(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    });
+
     // Global variables
     var dataBaseRef = firebase.database();
     
+
   	// selectors
   	var trainName = $("#trainName");
   	var destination = $("#destination");
